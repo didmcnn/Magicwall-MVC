f2852a5bdc9b33bd14333df3ed27550a995ede39
didmcnn <didemcan99@hotmail.com>
2024-11-18 17:04:14 +0300

    UI changed

---
+++ Magicwall/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magicwall.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: AdminController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
