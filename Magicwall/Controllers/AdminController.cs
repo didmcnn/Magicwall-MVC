e5816c63be729821920a16e5a181460b898d23d0
Eren <erencan.utku52@gmail.com>
2024-11-18 18:38:56 +0300

    UI changes have been made

--- Magicwall/Controllers/AdminController.cs
+++ Magicwall/Controllers/AdminController.cs
@@ -4,12 +4,49 @@ namespace Magicwall.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: AdminController
         public ActionResult Index()
         {
             return View();
         }
-
-
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult Catalog()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Documents()
+        {
+            return View();
+        }
+        public ActionResult JobApplication()
+        {
+            return View();
+        }
+        public ActionResult OpenPositions()
+        {
+            return View();
+        }
+        public ActionResult Models()
+        {
+            return View();
+        }
+        public ActionResult Photos()
+        {
+            return View();
+        }
+        public ActionResult Referances()
+        {
+            return View();
+        }
+        public ActionResult Videos()
+        {
+            return View();
+        }
     }
 }
