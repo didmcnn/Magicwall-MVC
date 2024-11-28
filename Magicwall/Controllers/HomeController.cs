096a78086c4c174cba0996796fa340c3461d3746
didmcnn <didemcan99@hotmail.com>
2024-11-28 22:37:28 +0300

    ModelsDetail.cshtml page added

--- Magicwall/Controllers/HomeController.cs
+++ Magicwall/Controllers/HomeController.cs
@@ -23,7 +23,8 @@ public class HomeController : Controller
     private readonly IBankAccountService _bankAccountService;
     private readonly IContactService _contactService;
     private readonly IJobApplicationService _jobApplicationService;
-    public HomeController(IOpenPositionService openPositionService,
+    public HomeController(
+        IOpenPositionService openPositionService,
         IHomePageItemService homePageItemService,
         IAboutService aboutService,
         IModelsService modelsService,
@@ -48,30 +49,45 @@ public class HomeController : Controller
         _jobApplicationService = jobApplicationService;
     }
 
+  #region Index
     public async Task<IActionResult> IndexAsync()
     {
         List<HomePageItem> homePageItems = await _homePageItemService.GetAllAsync();
         return View(homePageItems);
     }
+    #endregion
+
+    #region Error404
     public IActionResult Error404()
     {
         return View();
     }
+    #endregion
+
+    #region Privacy
     public IActionResult Privacy()
     {
         return View();
     }
+    #endregion
 
+    #region AboutUs 
     public async Task<IActionResult> AboutUsAsync()
     {
         List<About> aboutList = await _aboutService.GetAllAsync();
         return View(aboutList);
-    }
+        }
+    #endregion
+
+    #region Accounts
     public async Task<IActionResult> AccountsAsync()
     {
         List<BankAccount> bankAccounts = await _bankAccountService.GetAllAsync();
         return View(bankAccounts);
     }
+    #endregion
+
+    #region Contact
     public IActionResult Contact()
     {
         return View();
@@ -85,6 +101,9 @@ public class HomeController : Controller
         }
         return RedirectToAction("Contact");
     }
+    #endregion
+
+    #region Corporate   
     public IActionResult Corporate()
     {
         return View();
@@ -94,10 +113,16 @@ public class HomeController : Controller
         List<DocumentsPageItem> documentsPageItems = await _documentsPageItemService.GetAllAsync();
         return View(documentsPageItems);
     }
+    #endregion
+
+    #region Gallery 
     public IActionResult Gallery()
     {
         return View();
     }
+    #endregion
+
+    #region HumanResources
     public async Task<IActionResult> HumanResourcesAsync()
     {
         var pos = await _openPositionService.GetAllAsync();
@@ -121,29 +146,51 @@ public class HomeController : Controller
         }
         return RedirectToAction("HumanResources");
     }
+    #endregion
+
+    #region Models
     public async Task<IActionResult> ModelsAsync()
     {
         List<ModelPageItem> modelPageItems = await _modelsService.GetAllAsync();
         return View(modelPageItems);
     }
+    #endregion
+
+    #region ModelsDetail
+    public IActionResult ModelsDetail()
+    {
+        return View();
+    }
+    #endregion
+    #region PhotoGallery
     public async Task<IActionResult> PhotoGalleryAsync()
     {
         List<PhotoPageItem> photoPageItems = await _photoPageItemService.GetAllAsync();
         return View(photoPageItems);
     }
+    #endregion
+
+    #region VideoGallery
     public async Task<IActionResult> VideoGalleryAsync()
     {
         List<VideoPageItem> videoPageItems = await _videoPageItemService.GetAllAsync();
         return View(videoPageItems);
     }
+    #endregion
+
+    #region References
     public async Task<IActionResult> ReferencesAsync()
     {
         List<ReferencesPageItem> references = await _referencesPageItemService.GetAllAsync();
         return View(references);
     }
+    #endregion
+
+    #region Error
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    #endregion
 }
