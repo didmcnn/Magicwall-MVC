192b4d547cf679d3b4b47e2758483caacc670801
erencan utku <erencan.utku52@gmail.com>
2025-01-12 23:51:00 +0300

    migration

---
+++ DataAccessLayer/Migrations/20250112153706_modelDetailsUpdated.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelDetailsUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AltTitle",
+                table: "ModelDetails");
+
+            migrationBuilder.DropColumn(
+                name: "BendStrength",
+                table: "ModelDetails");
+
+            migrationBuilder.DropColumn(
+                name: "SubTitle",
+                table: "ModelDetails");
+
+            migrationBuilder.DropColumn(
+                name: "SurfaceFinish",
+                table: "ModelDetails");
+
+            migrationBuilder.DropColumn(
+                name: "SurfaceQuality",
+                table: "ModelDetails");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 1, 12, 15, 37, 5, 139, DateTimeKind.Utc).AddTicks(1031));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AltTitle",
+                table: "ModelDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BendStrength",
+                table: "ModelDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SubTitle",
+                table: "ModelDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SurfaceFinish",
+                table: "ModelDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SurfaceQuality",
+                table: "ModelDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 29, 22, 23, 1, 35, DateTimeKind.Utc).AddTicks(6789));
+        }
+    }
+}
